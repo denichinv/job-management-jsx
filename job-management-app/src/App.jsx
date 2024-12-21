deca7eed53de937d41e0436292362c0b1eba18d1
Vilizar Denichin <denichinv@gmail.com>
2024-12-21 16:01:14 +0000

    footer added and styled

--- job-management-app/src/App.jsx
+++ job-management-app/src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import "./Header"
 import Header from './Header'
 import "./index.css"
+import Footer from "./Footer";
 function App() {
 
 
   return (
     <>
   <Header/>
+  <Footer/>
     </>
   )
 }
---
+++ job-management-app/src/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+  return (
+    <div id="footer">
+      <h3>© 2024 TaskTrail Inc. All rights reserved.</h3>
+    </div>
+  )
+}
+
+export default Footer
