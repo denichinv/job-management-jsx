99676a9b38dba6b280fa91021ffe628cd34ac8bc
Vilizar Denichin <denichinv@gmail.com>
2025-01-11 16:46:53 +0000

    footer edit

--- job-management-app/src/Footer.jsx
+++ job-management-app/src/Footer.jsx
@@ -3,7 +3,7 @@
 const Footer = () => {
   return (
     <div id="footer">
-      <h3>© 2024 TaskTrail Inc. All rights reserved.</h3>
+      <h3>© 2024 JobFlow Inc. All rights reserved.</h3>
     </div>
   )
 }
